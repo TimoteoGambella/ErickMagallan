bacf3c99f4488913567346d261e89872eaddabff
Jorge Luis Moreno Moller <jlmm.jlmm76@gmail.com>
2024-04-15 20:46:58 -0500

    Despliegue de submenu con hover

--- src/components/NavBar/DesktopNavBar.jsx
+++ src/components/NavBar/DesktopNavBar.jsx
@@ -20,20 +20,14 @@ const DesktopNavbar = ({menu, subMenu}) => {
   };
 
   const handleItemClick = (item) => {
-    if (item.name === "Servicios") {
-      handleServiciosClick();
-    } else {
+    if (item.name !== "Servicios") {
       navigate(item.link);
       setDisplay("hidden"); // Ocultar el submenú al hacer clic en cualquier otro elemento del menú
     }
   };
 
-  const handleServiciosClick = () => {
-    if (display === "hidden") {
-      setDisplay("flex");
-    } else {
-      setDisplay("hidden");
-    }
+  const handleServiciosHover = (isHovering) => {
+    setDisplay(isHovering ? "flex" : "hidden");
   };
 
   const isServiciosPage = location.pathname.startsWith("/servicios");
@@ -52,19 +46,38 @@ const DesktopNavbar = ({menu, subMenu}) => {
           />
         </div>
         <ul className="hidden md:flex flex-row space-x-8 md:mt-0 md:font-medium">
-          {menu.map((item, index) => (
-            <li key={index} className="md:mr-5">
-              <p  
-                onClick={() => handleItemClick(item)}
-                className={`flex items-center py-2 pr-4 pl-3 cursor-pointer ${item.link === location.pathname || (item.name === "Servicios" && isServiciosPage) ? 'text-color2' : 'text-color6'} md:hover:bg-transparent text-16`}
-              >
-                {item.name}
+        {menu.map((item, index) => {
+            const liProps = {};
+            if (item.name === "Servicios") {
+              liProps.onMouseEnter = () => handleServiciosHover(true);
+              liProps.onMouseLeave = () => handleServiciosHover(false);
+            }
+            return (
+              <li key={index} className="md:mr-5" {...liProps}>
+                <p
+                  onClick={() => handleItemClick(item)}
+                  className={`flex items-center py-2 pr-4 pl-3 cursor-pointer ${
+                    item.link === location.pathname ||
+                    (item.name === "Servicios" && isServiciosPage)
+                      ? "text-color2"
+                      : "text-color6"
+                  } ${item.name.toLowerCase()} md:hover:bg-transparent text-16`}
+                >
+                  {item.name}
+                  {item.name === "Servicios" && (
+                    <FiChevronDown className="w-4 h-4 ml-1" />
+                  )}
+                </p>
                 {item.name === "Servicios" && (
-                  <FiChevronDown className="w-4 h-4 ml-1" />
+                  <SubMenu
+                    subMenu={subMenu}
+                    display={display}
+                    setDisplay={setDisplay}
+                  />
                 )}
-              </p>
-            </li>
-          ))}
+              </li>
+            );
+          })}
         </ul>
         <button
           onClick={scrollToBottom}
@@ -74,24 +87,34 @@ const DesktopNavbar = ({menu, subMenu}) => {
           Agenda una cita
         </button>
       </div>
-      <div>
-        <ul id="subMenu" className={`${display} bg-white items-center justify-center gap-4 py-4 flex-col md:flex-row text-center`}>
-          {subMenu.map((subItem, subIndex) => (
-            <li key={subIndex} className="cursor-pointer">
-              <p
-                onClick={() => {
-                  navigate(subItem.link);
-                  setDisplay("hidden"); // Ocultar el submenú al hacer clic en cualquier elemento del submenú
-                }}
-                className={`block font-[700] leading-16 font-lato py-2 pr-4 pl-3 cursor-pointer ${subItem.link === location.pathname ? 'text-color2' : 'hover:color2'} md:hover:bg-transparent text-16`}
-              >
-                {subItem.title}
-              </p>
-            </li>
-          ))}
-        </ul>
-      </div>
     </nav>
+  );
+};
+
+const SubMenu = ({ subMenu, display, setDisplay }) => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  return (
+    <div
+      className={`absolute w-screen max-w-full bg-white items-center justify-center gap-4 py-4 flex-col md:flex-row text-center left-0 flex ${
+        display === "flex" ? "z-10" : "hidden"
+      }`}
+    >
+      {subMenu.map((subItem, subIndex) => (
+        <p
+          key={subIndex}
+          onClick={() => {
+            navigate(subItem.link);
+          }}
+          className={`block font-[700] leading-16 font-lato py-2 pr-4 pl-3 cursor-pointer ${
+            subItem.link === location.pathname ? "text-color2" : "hover:color2"
+          } md:hover:bg-transparent text-16`}
+        >
+          {subItem.title}
+        </p>
+      ))}
+    </div>
   );
 };
 
