4119dce219c33026336881ced00f15f924ef31d1
ErickMagallan <arqeemg@gmail.com>
2024-05-02 23:51:11 -0600

    Alzheimer, Autismo y Ansiedad

--- src/publis.js
+++ src/publis.js
@@ -12,103 +12,103 @@ import photo10 from "./assets/publis/picture-10.svg";
 
 export const publis_tms = [
     {
-        title: "Reducción de síntomas en la enfermedad de Parkinson",
-        link: "https://www.ncbi.nlm.nih.gov/pubmed/25237116",
-        description: "Lorem ipsum dolor sit amet consectetur. Quisque consectetur in amet nunc vitae at enim feugiat. Vestibulum a suspendisse praesent aliquam at lacus.",
+        title: "TMS mejora la memoria y actividad neuronal en la enfermedad de Alzheimer",
+        link: "https://drive.google.com/file/d/1oOqd7QoBKDdtc-wsw5klJ4HJQkh8sCQL/view?usp=sharing",
+        description: "El análisis de la señal TMS-EEG reveló un aumento de la actividad neuronal en el CP de los pacientes, un aumento de las oscilaciones cerebrales en la banda beta y una modificación de las conexiones funcionales entre el CP y las áreas frontales mediales dentro de la DMN (...)",
         language: "Inglés",
         photo: photo1,
-        problem: "Parkinson",
-        author: "Actipulse"
+        problem: "Alzheimer",
+        author: "NeuroImage"
     },
     {
-        title: "Retardo en la progresión de la enfermedad de Parkinson",
-        link: "https://www.ncbi.nlm.nih.gov/pubmed/25237116",
-        description: "Lorem ipsum dolor sit amet consectetur. Quisque consectetur in amet nunc vitae at enim feugiat. Vestibulum a suspendisse praesent aliquam at lacus.",
+        title: "Mejora de la cognición en pacientes ancianos con deterioro cognitivo",
+        link: "https://drive.google.com/file/d/1C6glJ_JM9MicUPRePNMh6Cni5wqox7B2/view?usp=sharing",
+        description: "Efectos de la estimulación magnética transcraneal repetitiva en mejora de la cognición en pacientes ancianos con deterioro cognitivo: revisión sistemática y metaanálisis",
         language: "Inglés",
         photo: photo2,
-        problem: "Parkinson",
-        author: "Actipulse"
+        problem: "Alzheimer",
+        author: "International Journal of Geriatric Psychiatry"
     },
     {
-        title: "Remisión de síntomas depresivos con EMT",
-        link: "https://www.ncbi.nlm.nih.gov/pubmed/25237116",
-        description: "Lorem ipsum dolor sit amet consectetur. Quisque consectetur in amet nunc vitae at enim feugiat. Vestibulum a suspendisse praesent aliquam at lacus.",
+        title: "El uso de la Estimulación Magnética Gamma Rápida para el tratamiento del Alzheimer",
+        link: "https://drive.google.com/file/d/1OamdFi0H46pCTcNHiOuMkBoJ1YHd2T3M/view?usp=sharing",
+        description: "Uso de la Estimulación Magnética Gamma Rápida sobre la Corteza Dorsolateral Prefrontal Izquierda para el Tratamiento del DCL y la Enfermedad de Alzheimer Leve: Un estudio piloto doble ciego, aleatorizado, controlado con simulacro.",
         language: "Inglés",
         photo: photo3,
-        problem: "Depresión",
-        author: "Actipulse"
+        problem: "Alzheimer",
+        author: "Frontiers in Neurology"
     },
     {
-        title: "Efectos visibles de la EMT en Ansiedad",
-        link: "https://www.ncbi.nlm.nih.gov/pubmed/25237116",
-        description: "Lorem ipsum dolor sit amet consectetur. Quisque consectetur in amet nunc vitae at enim feugiat. Vestibulum a suspendisse praesent aliquam at lacus.",
+        title: "TMS para el deterioro cognitivo leve en ancianos - Estudio",
+        link: "https://drive.google.com/file/d/1_Ej6Re10XLIQLUOtcjqdE6IVpOJt8g4Q/view?usp=sharing",
+        description: "Estimulación magnética transcraneal para tratar el deterioro cognitivo leve en ancianos: Un estudio controlado aleatorizado",
         language: "Inglés",
         photo: photo4,
-        problem: "Ansiedad",
-        author: "Actipulse"
+        problem: "Alzheimer",
+        author: "Hindawi Publishing Corporation"
     },
     {
-        title: "Reducción de síntomas en el TDAH en adultos",
-        link: "https://www.ncbi.nlm.nih.gov/pubmed/25237116",
-        description: "Lorem ipsum dolor sit amet consectetur. Quisque consectetur in amet nunc vitae at enim feugiat. Vestibulum a suspendisse praesent aliquam at lacus.",
+        title: "Mejora de la función cognitiva con TMS en personas con Alzheimer",
+        link: "https://drive.google.com/file/d/14GXDINt8ullTTrfU371i2kmBnsqoe8jt/view?usp=sharing",
+        description: "La estimulación magnética transcraneal repetitiva (EMTr) actúa como un tipo de método ampliamente aplicado y no invasivo en la intervención de algunos trastornos neurológicos. Este ensayo prospectivo, aleatorizado, doble ciego y controlado (...)",
         language: "Inglés",
         photo: photo5,
-        problem: "TDAH",
-        author: "Actipulse"
+        problem: "Alzheimer",
+        author: "Oncotarget"
     },
     {
-        title: "Cambios significativos en disfagia por EVC",
-        link: "https://www.ncbi.nlm.nih.gov/pubmed/25237116",
-        description: "Lorem ipsum dolor sit amet consectetur. Quisque consectetur in amet nunc vitae at enim feugiat. Vestibulum a suspendisse praesent aliquam at lacus.",
+        title: "Reducción aguda de la ansiedad tras la Estimulación Magnética Transcraneal Profunda",
+        link: "https://drive.google.com/file/d/1IFh5UsJlrgjxZTbQTFfzWVW0x7YiBo8k/view?usp=sharing",
+        description: "El presente estudio investigó las propiedades ansiolíticas de la estimulación magnética transcraneal profunda (EMTD) en la depresión mayor unipolar mediante una revisión sistemática de la literatura y un metaanálisis.",
         language: "Inglés",
         photo: photo6,
-        problem: "EVC",
-        author: "Actipulse"
+        problem: "Ansiedad",
+        author: "Elsevier"
     },
     {
-        title: "Incremento de la actividad motora: Parkinson",
-        link: "https://www.ncbi.nlm.nih.gov/pubmed/25237116",
-        description: "Lorem ipsum dolor sit amet consectetur. Quisque consectetur in amet nunc vitae at enim feugiat. Vestibulum a suspendisse praesent aliquam at lacus.",
+        title: "Estimulación Magnética Transcraneal en la Corteza Prefrontal Lateral Derecha",
+        link: "https://drive.google.com/file/d/1vTpFahQQK7W23B-tok8GzSx5JaAAo_k6/view?usp=sharing",
+        description: "Clasificado como trastorno de ansiedad, el trastorno de ansiedad generalizada (TAG) por sí solo es una enfermedad crónica y debilitante con una prevalencia en un año de aproximadamente el 2% (...)",
         language: "Inglés",
         photo: photo7,
-        problem: "EVC",
-        author: "Actipulse"
+        problem: "Ansiedad",
+        author: "Elsevier"
     },
     {
-        title: "Mejora en discinesia por Evento Vascular Cerebral",
-        link: "https://www.ncbi.nlm.nih.gov/pubmed/25237116",
-        description: "Lorem ipsum dolor sit amet consectetur. Quisque consectetur in amet nunc vitae at enim feugiat. Vestibulum a suspendisse praesent aliquam at lacus.",
+        title: "Estimulación Magnética Transcraneal en la Ansiedad y los trastornos relacionados con el trauma",
+        link: "https://drive.google.com/file/d/1tMriIho9-qgWhn7STrOolbgrxfe7r49-/view?usp=sharing",
+        description: "La estimulación magnética transcraneal (EMT) se ha evaluado como una opción de tratamiento eficaz para pacientes con trastorno depresivo mayor. Sin embargo, hay pocos estudios que hayan evaluado la eficacia de la EMT para otros trastornos neuropsiquiátricos como la ansiedad y los trastornos relacionados con el trauma. Se revisó la literatura que ha evaluado la EMT como tratamiento para la ansiedad y los trastornos relacionados con el trauma.",
         language: "Inglés",
         photo: photo8,
-        problem: "EVC",
-        author: "Actipulse"
+        problem: "Ansiedad",
+        author: "Wiley"
     },
     {
-        title: "Eficacia para la función de las extremidades superiores: EVC",
-        link: "https://www.ncbi.nlm.nih.gov/pubmed/25237116",
-        description: "Lorem ipsum dolor sit amet consectetur. Quisque consectetur in amet nunc vitae at enim feugiat. Vestibulum a suspendisse praesent aliquam at lacus.",
+        title: "Tratamiento del espectro autista con la Estimulación Magnética Transcraneal",
+        link: "https://drive.google.com/file/d/1buBy4KCtD5e9j3D-19cfwtDnxMTIOGLd/view?usp=sharing",
+        description: "El Trastorno del Espectro Autista (TEA) es un trastorno del neurodesarrollo que se manifiesta como déficits de por vida en la comunicación e interacción social, así como comportamientos, intereses y actividades repetitivos restringidos.",
         language: "Inglés",
         photo: photo9,
-        problem: "EVC",
-        author: "Actipulse"
+        problem: "Autismo",
+        author: "Frontiers in Integrative Neuroscience"
     },
     {
-        title: "Mejora en memoria cotidiana",
+        title: "Estudio exploratorio de los efectos de la neuromodulación en los síntomas conductuales del autismo",
         link: "https://www.ncbi.nlm.nih.gov/pubmed/25237116",
-        description: "Lorem ipsum dolor sit amet consectetur. Quisque consectetur in amet nunc vitae at enim feugiat. Vestibulum a suspendisse praesent aliquam at lacus.",
+        description: "La estimulación magnética transcraneal repetitiva (EMTr) ofrece un medio no invasivo de modular el sesgo cortical E/I con escasos efectos secundarios. En este estudio, se reclutaron 124 niños con TEA de alto funcionamiento (...)",
         language: "Inglés",
         photo: photo10,
-        problem: "EVC",
-        author: "Actipulse"
+        problem: "Autismo",
+        author: "Frontiers in Systems Neuroscience"
     },
     {
-        title: "Efectos en Deterioro Cognitivo Leve",
-        link: "https://www.ncbi.nlm.nih.gov/pubmed/25237116",
-        description: "Lorem ipsum dolor sit amet consectetur. Quisque consectetur in amet nunc vitae at enim feugiat. Vestibulum a suspendisse praesent aliquam at lacus.",
+        title: "Estimulación Magnética Transcraneal aplicada en niños con espectro autista",
+        link: "https://drive.google.com/file/d/1sqylAoUeN0BE_wSaxz70xBh_RkN1ui5t/view?usp=sharing",
+        description: "La estimulación magnética transcraneal repetitiva (EMTr) es una técnica segura y eficaz para estimular áreas específicas de disfunción cortical en varias enfermedades neuropsiquiátricas; sin embargo, se desconoce si la EMTr de alta frecuencia (...)",
         language: "Inglés",
         photo: photo8,
-        problem: "EVC",
-        author: "Actipulse"
+        problem: "Autismo",
+        author: "Frontiers in Psychiatry"
     },
     {
         title: "Eficacia en depresión mayor",
